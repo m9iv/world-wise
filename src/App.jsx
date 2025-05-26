2677e691775389d747ed37f3e8ec81bc7b102ef2
m9iv <aethawalka@gmail.com>
2025-05-26 15:22:01 +0300

    Add basic pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import Login from './pages/Login'
+import AppLayout from './pages/AppLayout'
+import PageNotFound from './pages/PageNotFound'
+
 function App() {
-  return <div>World Wise</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
