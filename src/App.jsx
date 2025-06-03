577652314cde44bf75f50ff7f86c84ef05cea864
m9iv <aethawalka@gmail.com>
2025-06-03 15:00:41 +0300

    Implement programmatic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
+// PAGES
 import Homepage from './pages/Homepage'
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
 import Login from './pages/Login'
 import AppLayout from './pages/AppLayout'
 import PageNotFound from './pages/PageNotFound'
+
+// COMPONENTS
+import City from './components/City'
 import CityList from './components/CityList'
 import CountryList from './components/CountryList'
+import Form from './components/Form'
 
 const BASE_URL = 'http://localhost:9000'
 
@@ -41,23 +46,25 @@ function App() {
         <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
 
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
+
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+
+          <Route path="cities/:id" element={<City />} />
+
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+
+          <Route path="form" element={<Form />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,23 @@
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 function Map() {
-  return <div className={styles.mapContainer}></div>
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const lat = searchParams.get('lat')
+  const lng = searchParams.get('lng')
+
+  const navigate = useNavigate()
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate('form')}>
+      <h1>Map</h1>
+
+      <h2>
+        Position: {lat}, {lng}
+      </h2>
+    </div>
+  )
 }
 
 export default Map
